12079c913cac512853343bee9f6283a6b73b0d70
Mehmet Selçuk Güler <gercekselcukguler@gmail.com>
2023-07-04 00:32:12 +0300

    Declare Remove book function

--- scripts.js
+++ scripts.js
@@ -9,21 +9,22 @@ const form = document.querySelector(".add-book-form");
 function Book(title, author) {
   this.title = title;
   this.author = author;
-  this.id = Math.random().toString() //unique id declaration
+  this.id = (Math.floor(Math.random()*10000) +1).toString() //unique id declaration
 }
 
 const postContainer = document.querySelector(".book-list");
 
-function postMethods() {
+function loadBookSection() {
   //create
   const bookDB = JSON.parse(localStorage.getItem("bookData"));
+  console.log(bookDB);
   // check the local storage if there is any database for books list
   if (bookDB === null || bookDB.length === 0) {
     // exit before
+    postContainer.parentElement.removeChild(postContainer);
     return;
   }
   bookDB.forEach((book) => {
-    console.log(book);
     const postElement = document.createElement("article");
     postElement.classList.add("book-item");
     postElement.innerHTML = `
@@ -38,10 +39,11 @@ function postMethods() {
     removeBtn.id = book.id;
     postElement.appendChild(removeBtn);
 
+    removeBtn.addEventListener('click', removeBook)
   });
 }
 
-postMethods();
+loadBookSection();
 
 function addBook() {
   /* event.preventDefault(); */
@@ -52,10 +54,19 @@ function addBook() {
     const newBookData = [...bookDB, book];
     // bookData.push(book)
     localStorage.setItem("bookData", JSON.stringify(newBookData));
-    postMethods(newBookData);
-    console.log(newBookData);
+    // loadBookSection();
   }
   /* var newbookData = JSON.parse(localStorage.getItem('bookData')) */
+}
+
+function removeBook(event) {
+  console.log(event.target.id)
+  const id = event.target.id;
+  const bookDB = JSON.parse(localStorage.getItem('bookData'));
+  const newBookData = bookDB.filter((book) => book.id !== id);
+  // console.log(newBookData)
+  localStorage.setItem('bookData', JSON.stringify(newBookData));
+  window.location.reload();
 }
 
 form.addEventListener("submit", addBook);
