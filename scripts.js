9fa14ce1642206c72587db69db7133c75ca24d34
Mehmet Selçuk Güler <gercekselcukguler@gmail.com>
2023-07-03 22:00:00 +0300

    Fix add function issues - Add localstorage feature
    Declare local storage functions
    Fix add book function issues

--- scripts.js
+++ scripts.js
@@ -1,41 +1,56 @@
 //List Books
-const addButton = document.querySelector('.add-btn');
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
+const addButton = document.querySelector(".add-btn");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const form = document.querySelector(".add-book-form");
 
-const bookData = [];
+// localStorage.setItem('bookData', JSON.stringify());
 
-function Books(title, author) {
+function Book(title, author) {
   this.title = title;
   this.author = author;
-};
+}
 
-const postContainer = document.querySelector('.book-list');
-  
-const postMethods = () => {
-  bookData.forEach((book) => {
-    const postElement = document.createElement('article');
-    postElement.classList.add('book-item');
+const postContainer = document.querySelector(".book-list");
+
+function postMethods() {
+  const bookDB = JSON.parse(localStorage.getItem("bookData"));
+  if (bookDB === null || bookDB.length === 0) {
+    // exit before
+    return;
+  }
+  bookDB.forEach((book) => {
+    console.log(book);
+    const postElement = document.createElement("article");
+    postElement.classList.add("book-item");
     postElement.innerHTML = `
     <p class="book-title">${book.title}</p>
           <p class="book-author">${book.author}</p>
     `;
-    const removeBtn = document.createElement('button');
-    removeBtn.className = 'remove-btn';
-    removeBtn.innerText = 'Remove';
-    postElement.appendChild(removeBtn);
     postContainer.appendChild(postElement);
 
-    });
-  };
+    const removeBtn = document.createElement("button");
+    removeBtn.className = "remove-btn";
+    removeBtn.innerText = "Remove";
+    postElement.appendChild(removeBtn);
+  });
+}
 
-  function addBook() {
-    let book = new Books(titleInput.value, authorInput.value);
+postMethods();
 
-    if(titleInput.value && authorInput.value) {
-      bookData.push(book)
-    }
+function addBook() {
+  /* event.preventDefault(); */
+  /* if inputs are not empty */
+  const bookDB = JSON.parse(localStorage.getItem("bookData")) || [];
+  if (titleInput.value && authorInput.value) {
+    const book = new Book(titleInput.value, authorInput.value);
+    const newBookData = [...bookDB, book];
+    // bookData.push(book)
+    localStorage.setItem("bookData", JSON.stringify(newBookData));
+    postMethods(newBookData);
+    console.log(newBookData);
   }
+  /* var newbookData = JSON.parse(localStorage.getItem('bookData')) */
+}
 
-addButton.addEventListener('click', addBook);
-addButton.addEventListener('click', postMethods);
+form.addEventListener("submit", Book.addBook);
